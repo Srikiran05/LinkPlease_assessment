82c7f4ed84e89d666589c53092f7687401ef9e71
Srikiran05 <malladisrikiran@gmail.com>
2026-08-17 21:30:18 +0530

    Hardcode API_KEY to prevent Render Env variable space issues

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    API_KEY = os.environ.get('API_KEY', 'bWFsbGFkaXNyaWtpcmFuQGdtYWlsLmNvbQ.7c14e383d729c7fcad6a')
+    API_KEY = 'bWFsbGFkaXNyaWtpcmFuQGdtYWlsLmNvbQ.7c14e383d729c7fcad6a'
     PSEUDOGRAM_BASE_URL = os.environ.get('PSEUDOGRAM_BASE_URL', 'https://pseudogram-api.onrender.com')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///linkplease.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
